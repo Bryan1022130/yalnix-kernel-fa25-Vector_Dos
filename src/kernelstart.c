20c94dd02c18664b93454815e1a3ba4a75d19967
angel <angelcerv2006@gmail.com>
2025-10-23 15:13:52 -0400

    Change Trap handler and moved its buffer to main

--- src/kernelstart.c
+++ src/kernelstart.c
@@ -41,12 +41,16 @@ unsigned long int vp0 = VMEM_0_BASE >> PAGESHIFT;
 unsigned long int vp1 = VMEM_1_BASE >> PAGESHIFT;
 
 //Page Table allocation -> an array of page table entries
-static pte_t kernel_page_table[MAX_PT_ENTRY];
-static pte_t user_page_table[MAX_PT_ENTRY];
+//Gets Zeroed out since it is static and unintialized
+static pte_t kernel_page_table[MAX_PT_LEN];
+static pte_t user_page_table[MAX_PT_LEN];
 
 //Terminal Array
 //Functions for terminal {TTY_TRANSMIT && TTY_RECEIVE}
-unsigned int terminal_array[NUM_TERMINALS];
+static unsigned int terminal_array[NUM_TERMINALS];
+
+//Global array for the Interrupt Vector Table
+HandleTrapCall Interrupt_Vector_Table[TRAP_VECTOR_SIZE];
 
 /* Initializing Virtual Memory
  * char *cmd_args: Vector of strings, holding a pointer to each argc in boot command line {Terminated by NULL poointer}
@@ -56,7 +60,7 @@ unsigned int terminal_array[NUM_TERMINALS];
 
 void create_free_frames(void){
 	//Set all pages as unused for now 
-	unsigned short int bitmap[MAX_PT_ENTRY] = {0};
+	unsigned short int bitmap[MAX_PT_LEN] = {0};
 
 	//Maybe it can also be set in a unsigned long int???
 	unsigned long int bitmap = 0;
@@ -90,20 +94,17 @@ void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt){
 	 * _first_kernel_text_page - > low page of kernel text
 	 */
 
-	unsigned long int pfn_track = DOWN_TO_PAGE(_first_kernel_text_page); //Alias: Start of Text Segment
+	unsigned long int text_start = DOWN_TO_PAGE(_first_kernel_text_page); //Alias: Start of Text Segment
 									     
-	WriteRegister(REG_PTBR0, (unsigned int)pfn_track); //Write the address of the start of text for pte_t
+	WriteRegister(REG_PTBR0,(unsigned int)kernel_page_table); //Write the address of the start of text for pte_t
 
 	unsigned long int text_end = DOWN_TO_PAGE(_first_kernel_data_page);
 	
-	for(long int text = pfn_track; text < text_end; text++){
+	for(long int text = text_start; text < text_end; text++){
 		//Text section should be only have READ && EXEC permissions
 		kernel_page_table[text].prot = PROT_READ | PROT_EXEC;
 		kernel_page_table[text].valid = TRUE;
-		kernel_page_table[text].pfn = pfn_track;
-
-		//Increase by PAGESIZE to store the physical memory addr in pte_t.pfn
-		pfn_track+= PAGESIZE;
+		kernel_page_table[text].pfn = text;
 	}
 
 	/*Loop through heap and data sections and setup pte
@@ -111,37 +112,34 @@ void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt){
 	 * _orig_kernel_brk_page -> first unused page after kernel heap
 	 */
 
-
 	//Since the previous loop stopped right before the start of heap/data section
 	//We have to set this pfn manually here or else it would be mapped to 2 pte
 	//THIS COULD BE WRONG COME BACK AND CHECK THIS IF IT IS CORRECT
 	
 	unsigned long int heapdata_start = DOWN_TO_PAGE(_first_kernel_data_page); 
 	unsigned long int heapdata_end = DOWN_TO_PAGE(_orig_kernel_brk_page);
-	pfn_track = heapdata_start;
 
-	for(long int dataheap = heapdata_start; dataheap < heapdata_end; dataheap++){
+	for(long int data_heap = heapdata_start; data_heap < heapdata_end; data_heap++){
 		//Heap and Data section both have READ and WRITE conditions
-		kernel_page_table[dataheap].prot = PROT_WRITE | PROT_READ; 
-		kernel_page_table[dataheap].valid = TRUE;
-		kernel_page_table[dataheap].pfn = pfn_track;
-
-		//Increase by PAGESIZE as before
-		pfn_track += PAGESIZE;
+		kernel_page_table[data_heap].prot = PROT_WRITE | PROT_READ; 
+		kernel_page_table[data_heap].valid = TRUE;
+		kernel_page_table[data_heap].pfn = data_heap;
 	}
-	unsigned long int stackstart = DOWN_TO_PAGE(KERNEL_STACK_BASE);
-	unsigned long int stackend = DOWN_TO_PAGE(KERNEL_STACK_LIMIT);
 
-	for(long int stackloop = stackstart; stackloop < stackend; stackloop++){
-		kernel_page_table[stackloop].prot = PROT_READ | PROT_WRITE;
-		kernel_page_table[stackloop].valid = TRUE;
-		kernel_pag_table[stackloop].pfn = 
+	unsigned long int stack_start = DOWN_TO_PAGE(KERNEL_STACK_BASE);
+	unsigned long int stack_end = DOWN_TO_PAGE(KERNEL_STACK_LIMIT);
+
+	for(long int stack_loop = stack_start; stack_loop < stack_end; stack_loop++){
+		kernel_page_table[stack_loop].prot = PROT_READ | PROT_WRITE;
+		kernel_page_table[stack_loop].valid = TRUE;
+		kernel_page_table[stack_loop].pfn = stack_loop; 
 
 	}
 
-	//Write the page table table base and limit registers for Region 0
-	//In this case since we been tracking it with "pfn_track" we can just pass in value
-	WriteRegister(REG_PTLR0, (unsigned int)pfn_track);
+	//Write the page table table limit register for Region 0
+	//We can pass in heapdata_end because REG_PTLR0 needs number of entries in the page table for region 0
+	//MAX_PT_ENTRY {maybe} 
+	WriteRegister(REG_PTLR0, (unsigned int)MAX_PT_LEN);
 
 
 	//Interrupt the Vector table
@@ -160,6 +158,8 @@ void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt){
 
 	//Set the global variable as true 
 	vm_enabled = TRUE;
+
+
 
 	//Write the page table table base and limit registers for Region 1
 	WriteRegister(REG_PTBR1, (unsigned int) );
--- src/trap.c
+++ src/trap.c
@@ -7,69 +7,54 @@
 #include <ylib.h>
 #include <yuser.h>
 #include <sys/mman.h> // For PROT_WRITE | PROT_READ | PROT_EXEC
-#include "Queue.h"
+#include "trap.h"
 
 //typedef for function pointer that will point to the Handle Trap function
 typedef void (*HandleTrapCall)(UserContext *CurrUC);
 
-void setup_trap_handler(void){
+//Declaration of the default place holder function
+void HandleTrap(UserContext *CurrUC){
+	TracePrintf(0, "This is a placeholder function\n");
+	return;
+}
 
-	//Array of function pointers for handling traps
-	HandleTrapCall Interrupt_Vector_Table[TRAP_VECTOR_SIZE];
+/* Trap Handlers for Check Point 2
+ * -> TRAP_CLOCK
+ * -> TRAP_KERNEL
+ */
+
+void HandleClockTrap(UserContext *CurrUC){
+	TracePrintf(0, "In HandleClockTrap");
+
+	TracePrintf(0, "Leaving HandleKernelTrap");
+	return;
+}
+
+void HandleKenelTrap(UserContext *CurrUC){
+	TracePrintf(0, "In HandleKernelTrap");
+
+	TracePrintf(0, "Leaving HandleKernelTrap");
+	return;
+}
+
+void setup_trap_handler(HandleTrapCall Interrupt_Vector_Table[]){
 
 	//Setup the table!
 	for(int i = 0; i < TRAP_VECTOR_SIZE; i++){
 		Interrupt_Vector_Table[i] = &HandleTrap;
 	}
 
+	Interrupt_Vector_Table[TRAP_KERNEL] = &HandleKernelTrap;
+	Interrupt_Vector_Table[TRAP_CLOCK] = &HandleClockTrap;
+  	Interrupt_Vector_Table[TRAP_ILLEGAL] = &HandleIllegalTrap;
+    	Interrupt_Vector_Table[TRAP_MEMORY] = &HandleMemoryTrap;
+    	Interrupt_Vector_Table[TRAP_MATH] = &HandleMathTrap;
+    	Interrupt_Vector_Table[TRAP_TTY_RECEIVE] = &HandleReceiveTrap;
+    	Interrupt_Vector_Table[TRAP_TTY_TRANSMIT] = &HandleTransmitTrap;
+    	Interrupt_Vector_Table[TRAP_DISK] = &HandleDiskTrap;
+
 	//Write the Memory Address to the REG_VECTOR_BASE register so it knows where it is
-	WriteRegister(REG_VECTOR_BASE, (unsigned int)&Interrupt_Vector_Table);
+	WriteRegister(REG_VECTOR_BASE, (unsigned int)Interrupt_Vector_Table);
 }
 
-//Find the correct case for the Trap type, then call a function to handle it
-void HandleTrap(UserContext *CurrUC){
-
-        //Copy the incoming user context into the PCB
-        CopyUserContext();
-	
-	//The code field in the CurrUC is used to index into the interrupt table
-        switch(CurrUC-> code){
-                case TRAP_KERNEL:
-                        HandleKernelTrap(CurrUC);
-                        break;
-
-                case TRAP_CLOCK:
-                        HandleClockTrap(CurrUC);
-                        break;
-
-                case TRAP_ILLEGAL:
-                        HandleIllegalTrap(CurrUC);
-                        break;
-                case TRAP_MEMORY:
-                        HandleMemoryTrap(CurrUC);
-                        break;
-
-                case TRAP_MATH:
-                        HandleMathTrap(CurrUC);
-                        break;
-
-                case TRAP_TTY_RECEIVE:
-                        HandleReceiveTrap(CurrUC);
-                        break;
-
-                case TRAP_TTY_TRANSMIT:
-                        HandleTransmitTrap(CurrUC);
-                        break;
-
-                case TRAP_DISK:
-                        HandleDiskTrap(CurrUC);
-                        break;
-
-                default:
-                        PrintTracef(0, "That was an invalid TrapValue");
-                        break;
-        }
-
-        UpdateUserContext();
-}
 
