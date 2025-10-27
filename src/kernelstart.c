49cc9a9666512c4781063d64b0c9c1c565d7b0c4
angel <angelcerv2006@gmail.com>
2025-10-26 23:09:01 -0400

    New changes

--- src/kernelstart.c
+++ src/kernelstart.c
@@ -104,7 +104,7 @@ PCB *pcb_alloc(void){
 			process_table[pid].currState = READY; 
 			process_table[pid].pid = pid;
 			
-			TracePrintf(0, "We are going to return to you a free process with PID --> %d", pid);
+			TracePrintf(0, "We are going to return to you a free process with PID --> %d\n", pid);
 			return &process_table[pid];
 		}
 	}
@@ -335,12 +335,22 @@ void init_proc_create(void){
 
 }
 
+
+
+
+
+
+
+
+
 /* ========================================================
  * SetKernelBrk Function Logic
  * ========================================================
  */ 
 
 int SetKernelBrk(void * addr){
+	TracePrintf(0, "We are in the function SetKernelBrk\n");
+
 	//Convert the address to uintptr_t to be able to use as a integer
 	uintptr_t new_kbrk_addr = (uintptr_t)addr;
 	uintptr_t old_kbrk = (uintptr_t)current_kernel_brk;
@@ -350,30 +360,29 @@ int SetKernelBrk(void * addr){
 	 */
 	
 	if(vm_enabled == FALSE){
+		TracePrintf(0, "THIS IS CALLED WHEN VIRTUAL MEMORY IS NOT ENABLED\n");	
+
+		//Normalize byte address for brk_addr
 		uintptr_t original_brk_addr = (uintptr_t)_orig_kernel_brk_page * PAGESIZE;
 
-		//It can not be less then the original break point since this space is used for data
+		//It can not be less then the current break point since this space is used for data
 		if(new_kbrk_addr < old_kbrk){
-			TracePrintf(0, "You can not shrink the heap!");
+			TracePrintf(0, "You can not shrink the heap!\n");
 			return ERROR;
 		}
 
-		//Here check for more conditions that would be invalid, like if the new_kbrk_addr were to be greater than memory there is
-		// --- address validity checks before VM is enabled ---
-		uintptr_t heap_start = (uintptr_t)_orig_kernel_brk_page;
+		uintptr_t heap_start = (uintptr_t)_orig_kernel_brk_page * PAGESIZE;
 		uintptr_t heap_end_limit = (uintptr_t)KERNEL_STACK_BASE;
 
 		// can't shrink below data/heap start
 		if (new_kbrk_addr < heap_start) {
-		    TracePrintf(0, "[SetKernelBrk] Error: address %p is below kernel heap start (%p)\n",
-		                (void*)new_kbrk_addr, (void*)heap_start);
+		    TracePrintf(0, "[SetKernelBrk] Error: address %p is below kernel heap start (%p)\n", (void*)new_kbrk_addr, (void*)heap_start);
 		    return ERROR;
 		}
 
 		// can't grow into kernel stack region
 		if (new_kbrk_addr >= heap_end_limit) {
-		    TracePrintf(0, "[SetKernelBrk] Error: address %p would overlap kernel stack (%p)\n",
-		                (void*)new_kbrk_addr, (void*)heap_end_limit);
+		    TracePrintf(0, "[SetKernelBrk] Error: address %p would overlap kernel stack (%p)\n", (void*)new_kbrk_addr, (void*)heap_end_limit);
 		    return ERROR;
 		}
 
@@ -384,21 +393,18 @@ int SetKernelBrk(void * addr){
 		    return ERROR;
 		}
 
-		// 4. warn if not page aligned
-		if ((new_kbrk_addr & PAGEOFFSET) != 0) {
-		    TracePrintf(1, "[SetKernelBrk] Warning: unaligned address %p; rounding internally.\n",
-		                (void*)new_kbrk_addr);
-		}
-
 		current_kernel_brk = (void *)new_kbrk_addr;
+
 		TracePrintf(1, "[SetKernelBrk] Updated current_brk (no VM): %p\n", current_kernel_brk);
         	return 0;
 	}
 	else if (vm_enabled == TRUE){
+		TracePrintf(0, "VIRTUAL MEMORY HAS BEEN ENABLED \n");
+
 		//SetKernelBrk functions as regular brk after Virtual Memory has been initialized
 		
 		//Stores the byte address of the kernel break
-		uintptr_t original_brk_addr = (uintptr_t)_orig_kernel_brk_page;
+		uintptr_t original_brk_addr = (uintptr_t)_orig_kernel_brk_page * PAGESIZE;
 
 		if(new_kbrk_addr < original_brk_addr){
 			TracePrintf(0, "Error! I will be writing into the data section!");
@@ -407,32 +413,26 @@ int SetKernelBrk(void * addr){
 		
 		//Check if the requested new address space for the Kernel Heap Brk is valid
 
-		// --- address validity checks ---
-		uintptr_t heap_start = (uintptr_t)_orig_kernel_brk_page;
+		uintptr_t heap_start = (uintptr_t)_orig_kernel_brk_page * PAGESIZE;
 		uintptr_t heap_end_limit = (uintptr_t)KERNEL_STACK_BASE;
+
+
+		TracePrintf(1, "[SetKernelBrk-VM] Comparing new_brk: %p against heap_start: %p\n",
+                    (void*)new_kbrk_addr, (void*)heap_start);
+
 
 		// 1. must not go below heap start (data section)
 		if (new_kbrk_addr < heap_start) {
-		    TracePrintf(0, "[SetKernelBrk] Error: address %p is below kernel heap start (%p)\n",
-		                (void*)new_kbrk_addr, (void*)heap_start);
+		    TracePrintf(0, "[SetKernelBrk] Error: address %p is below kernel heap start (%p)\n", (void*)new_kbrk_addr, (void*)heap_start);
 		    return ERROR;
 		}
 
 		// 2. must not grow into kernel stack
 		if (new_kbrk_addr >= heap_end_limit) {
-		    TracePrintf(0, "[SetKernelBrk] Error: address %p overlaps kernel stack base (%p)\n",
-		                (void*)new_kbrk_addr, (void*)heap_end_limit);
+		    TracePrintf(0, "[SetKernelBrk] Error: address %p overlaps kernel stack base (%p)\n", (void*)new_kbrk_addr, (void*)heap_end_limit);
 		    return ERROR;
 		}
 
-		// 3. warn if not page aligned
-		if ((new_kbrk_addr & PAGEOFFSET) != 0) {
-		    TracePrintf(1, "[SetKernelBrk] Warning: unaligned address %p; rounding to page boundary.\n",
-		                (void*)new_kbrk_addr);
-		}
-
-		// Step 1: Align old/new break values
-		// ---------------------------------------------------------
 		uintptr_t grow_start = UP_TO_PAGE(old_kbrk);        // first new page if growing
 		uintptr_t grow_end   = UP_TO_PAGE(new_kbrk_addr);   // one past last page
 							
@@ -441,10 +441,12 @@ int SetKernelBrk(void * addr){
 
 		// Step 2: Growing the heap (allocate frames)
 		if (grow_end > grow_start) {
-		        TracePrintf(1, "[SetKernelBrk] Growing kernel heap...\n");
-		        uintptr_t mapped_until = grow_start;   // track how far we’ve gotten
+		        TracePrintf(1, "[SetKernelBrk] Growing kernel heap\n");
+
+		        uintptr_t mapped_until = grow_start;
 
 		        for (uintptr_t vaddr = grow_start; vaddr < grow_end; vaddr += PAGESIZE) {
+
 		                int vpn = (int)(vaddr >> PAGESHIFT);
 
 		                // skip if already valid 
@@ -453,7 +455,8 @@ int SetKernelBrk(void * addr){
 		                        continue;
 		                }
 
-		                int pfn = frame_alloc(-1);   // -1 marks kernel ownership
+		                int pfn = frame_alloc(0);
+
 		                if (pfn < 0) {
 		                        // roll back anything we just mapped
 		                        for (uintptr_t rv = grow_start; rv < mapped_until; rv += PAGESIZE) {
@@ -471,7 +474,7 @@ int SetKernelBrk(void * addr){
 
 		                kernel_page_table[vpn].pfn   = pfn;
 		                kernel_page_table[vpn].prot  = PROT_READ | PROT_WRITE;
-		                kernel_page_table[vpn].valid = 1;
+		                kernel_page_table[vpn].valid = TRUE;
 
 		                mapped_until = vaddr + PAGESIZE;
 		        }
@@ -500,46 +503,53 @@ int SetKernelBrk(void * addr){
 	        return 0;
 	    }
 	TracePrintf(0, "[SetKernelBrk] Unknown VM state!\n");
-	return -1;
+	return ERROR;
 }
 
-/* ========================================================
+
+
+
+
+
+
+
+
+
+/* ==========================================================================================================================================================
  * Initializing Virtual Memory
  * char *cmd_args: Vector of strings, holding a pointer to each argc in boot command line {Terminated by NULL poointer}
  * unsigned int pmem_size: Size of the physical memory of the machine {Given in bytes}
  * UserContext *uctxt: pointer to an initial UserContext structure
- * =========================================================
+ * ==========================================================================================================================================================
  */ 
 
 void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt){
 
+	//Calculate the number of page frames and store into our global variable 
+	frame_count = (pmem_size / PAGESIZE);
 
-	TracePrintf(0,"WE ARE CURRENTLY IN THE KERNELSTART PROGRAM\n");
-
-	TracePrintf(0, "<<<<<<<<<<<<<<<< this is the size og pmem_size => %lx", pmem_size);
+	TracePrintf(0,"========> This is the start of the KernelStart function <=====================\n");
+	TracePrintf(0, "<<<<<<<<<<<<<<<< this is the size pmem_size => %lX and this is the stack frames ==> %d >>>>>>>>>>>>>>>>>>>>>>>>\n", pmem_size, frame_count);
 
 	/* <<<---------------------------------------------------------
 	 * Boot up the free frame data structure && definee global vars
 	 * ---------------------------------------------------------->>>
 	 */
 
-	//Calculate the number of page frames and store into our global variable 
-	frame_count = pmem_size / PAGESIZE;
-	
 	//Initialize the data struct to track the free frames
 	frames_init(pmem_size);
 
-	//Set up global variable for UserContext
+	//Set up global variable for the current UserContext
 	KernelUC = uctxt;
 
 	/* <<<------------------------------
 	 * Set up the Interrupt Vector Table
 	 * ------------------------------>>>
 	 */
-	
-	TracePrintf(0,"WE ARE SETTING UP THE INTERRUPTM VERCTOR TABLE\n");
-	setup_trap_handler(Interrupt_Vector_Table);
 
+	TracePrintf(0,"+++++ We are setting up the IVT and im going to call the function\n");
+	setup_trap_handler(Interrupt_Vector_Table);
+	TracePrintf(0,"+++++ We have left the functions and going to set up region 0\n");
 
 	/* <<<---------------------------------------
 	 * Set up the initial Region 0 {KERNEL SPACE}
@@ -557,21 +567,19 @@ void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt){
 	 * _first_kernel_text_page - > low page of kernel text
 	 */
 	
-	TracePrintf(0,"WE ARE WRITING THE BASE REGISTER FOR REGION 0\n");
+	TracePrintf(0,"------------------------------------------------- Region 0 Set up ----------------------------------------------------\n");
 
 	//Set the Global variable
 	kernel_region_pt = (void *)kernel_page_table;
-
-
+	TracePrintf(0,"This is where my array is located --> %p\n", kernel_region_pt);
 	
 	unsigned long int text_start = _first_kernel_text_page;
-	TracePrintf(0, "This is the value of the text_page --> %lx and this is the of with the DOWN_TO_PAGE --> %lx +++++++++++++\n", _first_kernel_text_page, text_start);
-
+	TracePrintf(0, "This is the value of the text_start --> %lx ++++++++++++++= \n", text_start);
 	unsigned long int text_end = _first_kernel_data_page;
-
-	TracePrintf(0, "This is the value of the text_end ---> %lx\n ++++++++++++++", text_end);
+	TracePrintf(0, "This is the value of the text_end ---> %lx++++++++++++++\n", text_end);
 	
 	for(unsigned long int text = text_start; text < text_end; text++){
+
 		//Text section should be only have READ && EXEC permissions
 		kernel_page_table[text].prot = PROT_READ | PROT_EXEC;
 		kernel_page_table[text].valid = TRUE;
@@ -584,10 +592,9 @@ void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt){
 	 */
 
 	unsigned long int heapdata_start = _first_kernel_data_page; 
-	TracePrintf(0, "This is the value of the heapdata_start---> %lx\n ++++++++++++++++", heapdata_start);
-
+	TracePrintf(0, "This is the value of the heapdata_start---> %lx++++++++++++++++\n", heapdata_start);
 	unsigned long int heapdata_end = _orig_kernel_brk_page;
-	TracePrintf(0, "This is the value of the heapdata_end ---> %lx\n ++++++++++++++", heapdata_end);
+	TracePrintf(0, "This is the value of the heapdata_end ---> %lx ++++++++++++++++++++\n", heapdata_end);
 
 	for(unsigned long int data_heap = heapdata_start; data_heap < heapdata_end; data_heap++){
 		//Heap and Data section both have READ and WRITE conditions
@@ -595,51 +602,44 @@ void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt){
 		kernel_page_table[data_heap].valid = TRUE;
 		kernel_page_table[data_heap].pfn = data_heap;
 	}
-	TracePrintf(0,"WE ARE GETTING IN THE RED ZONE PAGE\n");
-
 
 	/* ==============================
 	 * Red Zone {Unmapped Pages}
 	 * ==============================
 	 */
 
-	TracePrintf(0,"WE ARE GETTING IN THE RED ZONE PAGE\n");
-
 	unsigned long int stack_start = KERNEL_STACK_BASE >> PAGESHIFT;
 	unsigned long int stack_end = KERNEL_STACK_LIMIT >> PAGESHIFT;
 	
-	TracePrintf(0,"This is in the stack loop\n");
-
-
+	TracePrintf(0,"This is in the stack loop logic\n");
 	TracePrintf(0," this is the value of stack_start --> %lx and this is stackend --> %lx \n", stack_start, stack_end);
 
 	for(unsigned long int stack_loop = stack_start; stack_loop < stack_end; stack_loop++){
-
 		TracePrintf(0,"WE ARE in the stack loop\n");
 		kernel_page_table[stack_loop].prot = PROT_READ | PROT_WRITE;
 		kernel_page_table[stack_loop].valid = TRUE;
 		kernel_page_table[stack_loop].pfn = stack_loop; 
 	}
 
-	TracePrintf(0,"THIS IS BEFORE WE ARE WRITING THE PAGE TABLE ADRESSSSSSSSS\n");
+	TracePrintf(0,"THIS IS BEFORE WE ARE WRITING THE PAGE TABLE ADRESSS TO THE HARDWARE\n");
 
-	//Set global variable for stack limit
-	kernel_stack_limit = (void *)VMEM_0_LIMIT;
+	//Set global variable for stack limit for region 0
+	kernel_stack_limit = (void *)KERNEL_STACK_LIMIT;
 
 	//Write the address of the start of text for pte_t
 	WriteRegister(REG_PTBR0,(unsigned int)kernel_page_table);
 	
-	//Write the page table table limit register for Region 0
 	//MAX_PT_LEN because REG_PTLR0 needs number of entries in the page table for region 0
 	WriteRegister(REG_PTLR0, (unsigned int)MAX_PT_LEN);
 
-	TracePrintf(0,"WE ARE DONE SETTTING UP THE PAGE TABLES FOR STACK, TEXT AND DATA\n");
+	TracePrintf(0,"|||||||||||||||||||||| WE ARE DONE SETTTING UP THE PAGE TABLES FOR STACK, TEXT AND DATA ||||||||||||||||||||||||||||||||||||||||||\n");
 
 	/* <<<--------------------------
 	 * Initialize Virtual Memory
 	 * -------------------------->>>
 	 */
-		
+	
+	TracePrintf(0, "------------------------------------- WE ARE TURNING ON VIRTUAL MEMORY NOW :) -------------------------------------\n");
 	//Write to special register that Virtual Memory is enabled 
 	WriteRegister(REG_VM_ENABLE, TRUE);
 
@@ -652,22 +652,32 @@ void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt){
 	 */
 
 	//Set current brk and then call SetKernelBrk
-	
-    	uintptr_t orig_brk_address = (uintptr_t)_orig_kernel_brk_page * PAGESIZE;
+	TracePrintf(0, "------------------------------------- WE ARE NOW GOING TO CALL KERNELBRK :) -------------------------------------\n");
 
+	//Normalize into byte address
+    	uintptr_t orig_brk_address = (uintptr_t)_orig_kernel_brk_page * PAGESIZE;
 	current_kernel_brk = (void *)orig_brk_address;
+
+	TracePrintf(1, "KernelStart: Initializing kernel heap manager...\n"
+                       "KernelStart: Initial kernel brk address calculated as: %p\n"
+                       "KernelStart: Calling SetKernelBrk(%p) to set initial heap top.\n",
+                       (void *)orig_brk_address, current_kernel_brk);
 
 	int kbrk_return = SetKernelBrk(current_kernel_brk);
 
 	if(kbrk_return != 0){
-		TracePrintf(0, "There was an error in SetKernelBrk");
+		TracePrintf(0, "There was an error in SetKernelBrk\n");
 		return;
 	}
+
+	TracePrintf(0, "We have called set KernelBrk : )\n");
+
 
 	/* <<<-------------------------------------
 	 * Create Process
 	 * ------------------------------------->>>
 	 */
+	TracePrintf(0, "------------------------------------- WE ARE NOW GOING TO CREATE OUR PROCESS :) -------------------------------------\n");
 
 	//Create idle proc
 	InitPcbTable();
