88eeec8e857b1038504983305ccda3a9b3d227ec
angel <angelcerv2006@gmail.com>
2025-11-01 19:25:10 -0400

    Cleaning up and works also:

--- src/kernelstart.c
+++ src/kernelstart.c
@@ -67,9 +67,6 @@ HandleTrapCall Interrupt_Vector_Table[TRAP_VECTOR_SIZE];
  * ==================================================================================================================
  */
 
-//We dont need to call kernel s frame for idle process
-//Idle process first 
-//init second 
 int create_sframes(PCB *free_proc, unsigned char *track, int track_size){
         TracePrintf(0, "Creating the Kernel Stack for the Process this many --> %d\n", KERNEL_STACK_PAGES);
         for(int i = 0; i < KERNEL_STACK_PAGES; i++){
@@ -96,84 +93,70 @@ int create_sframes(PCB *free_proc, unsigned char *track, int track_size){
  */
 
 void idle_proc_create(unsigned char *track, int track_size, pte_t *user_page_table, UserContext *uctxt){
-	TracePrintf(1, "idle_proc_create(): begin\n");
+	TracePrintf(0, "Start of the idle_proc_create function <|> \n");
 
 	//Malloc space for PCB idle struct 
-	TracePrintf(0, " We are going to call idle_process");
 	PCB *idle_process = (PCB *)malloc(sizeof(PCB));
 
-	
-	TracePrintf(0, "About to get idle_pt\n");
+	//Point to our user_page_table and clear the table 
 	pte_t *idle_pt = user_page_table;	
 	memset(idle_pt, 0, sizeof(pte_t) * MAX_PT_LEN);	
+
+	//Get a pid from the help of hardware
   	int pid_find = helper_new_pid(user_page_table);
 	idle_process->pid = pid_find;
-	TracePrintf(0, "Assigned idle_process->pid = %d\n", idle_process->pid);
 
+	//Allocate a physical page for the process
 	int pfn = find_frame(track, track_size);
 	if(pfn == ERROR){
-		TracePrintf(0, "There was an error with find_frame");
+		TracePrintf(0, "No frames were found!\n");
 		return;
 	}
-
-//	We are accessing region 1 page table
+	
+	//We are storing it at the top of the user stack region 
     	unsigned long stack_page_index = MAX_PT_LEN - 1;
 	idle_pt[stack_page_index].valid = TRUE;
    	idle_pt[stack_page_index].prot = PROT_READ | PROT_WRITE;
    	idle_pt[stack_page_index].pfn = pfn;
 
 	/* =======================
-	 * idle_proc setup
+	 * idle_process field setup
 	 * =======================
 	 */
-	
+
 	idle_process->AddressSpace = user_page_table;
-
 	idle_process->curr_uc.pc = (void*)DoIdle;
-	idle_process->curr_uc.sp = (void*)(VMEM_1_LIMIT - 4);
+	idle_process->curr_uc.sp = (void*)(VMEM_1_LIMIT - 1);
+	idle_process->currState = READY;
 
-	create_sframes(idle_process, track, track_size);
-	
-	TracePrintf(0, "This is the value of the idle pc -- > %p and this is the value of the sp --> %p\n", idle_process->curr_uc.pc, idle_process->curr_uc.sp);
-	TracePrintf(0, "idle_process->pid: %d\n", idle_process->pid);
-	
-	/* ======================================================
-	 * Copy in idle PCB properties into the KernelUC
-	 * ======================================================
-	 */
-
+	//Copy in idle PCB properties into the KernelUC
 	memcpy(KernelUC, &idle_process->curr_uc, sizeof(UserContext));
 
 	/* =======================================
-	 * Store AddressSpace for region 1 table
+	 * Write region 1 table to Hardware
 	 * =======================================
 	 */
 
-	//WriteRegister(REG_PTBR1, (unsigned int)(temp_vpn << PAGESHIFT));
 	WriteRegister(REG_PTBR1, (unsigned int)user_page_table);
 	WriteRegister(REG_PTLR1, (unsigned int)MAX_PT_LEN);
-	
-	//Flush for region 1 since we just wrote its start and limit
 	WriteRegister(REG_TLB_FLUSH, TLB_FLUSH_1);
-
-	//Set as running
-	idle_process->currState = READY;
-
+	
+	//write the current UserContext back into uctxt
 	memcpy(uctxt, &idle_process->curr_uc, sizeof(UserContext));
 
 	//Set global variable for current process as the idle process
 	current_process = idle_process;
 
 	TracePrintf(0, "===+++++++++++++++++++++++++ IDLE PROCESS DEBUG +++++++++++++++++++++++++++++++++++++++++++====\n");
-	TracePrintf(0, " This is the num of the array for the kernel_page_table --> %d", MAX_PT_LEN); 
+	TracePrintf(0, " This is the num of the array for the kernel_page_table --> %d\n", MAX_PT_LEN); 	
+	TracePrintf(0, "This is idle pc -- > %p and this is sp --> %p\n", idle_process->curr_uc.pc, idle_process->curr_uc.sp);
 	TracePrintf(0, "idle_process ptr =------------->>> : %p\n", idle_process);
 	TracePrintf(0, "idle_process->pid: %d\n", idle_process->pid);
-	TracePrintf(0, "This should be a reference to kernel page since the kernel does not interact with physical memory directly idle_process->AddressSpace: %p\n", idle_process->AddressSpace);
 	TracePrintf(0, "This is the value of the VMEM_1_LIMIT ==> %p and this is the VMEM_1_BASE ==> %p\n", VMEM_1_LIMIT, VMEM_1_BASE);
-	TracePrintf(0, "current_process ptr: %p\n", current_process);
-	TracePrintf(0, "==========================\n");
-	
-	TracePrintf(0, "idle_proc_create(): done (pid=%d, aspace=%p)\n", idle_process->pid, idle_process->AddressSpace);
+	TracePrintf(0, "current_process ptr: %p\n", current_process);	
+	TracePrintf(0, "(This is our region 1 Addressspace= %p\n", idle_process->AddressSpace);
+	TracePrintf(0, "===============================================================================================\n");
+	TracePrintf(0, "End of the idle_proc_create function <|> \n\n");
 }
 
 /* ==========================================================================================================================================================
@@ -185,20 +168,21 @@ void idle_proc_create(unsigned char *track, int track_size, pte_t *user_page_tab
  */ 
 
 void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt){
-	TracePrintf(0, "--------> We are the start of the KernelStart function <-----------");
+	TracePrintf(0, "---------> We are the start of the KernelStart function <-----------");
 
 	/* <<<---------------------------------------------------------
 	 * Boot up the free frame data structure && define global vars
 	 * ---------------------------------------------------------->>>
 	 */
 	
-	//Calculate the number of page frames and store into our global variable 
+	//Calculate number of physical frames  
 	frame_count = (pmem_size / PAGESIZE);
 
+	//Free frames creation
 	unsigned char *track = (unsigned char*)malloc(frame_count);
 	init_frames(track, frame_count);
 
-	// initialize process queues
+	//initialize process queues
 	readyQueue = initializeQueue();
 	sleepQueue = initializeQueue();
 	
@@ -208,36 +192,30 @@ void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt){
 	/* <<<---------------------------------------
 	 * Set up the initial Region 0 {KERNEL SPACE}
 	 * Build Region 0 mappings for kernel text, data, heap, and stack.  
-	 * -->Stack	-
+	 * --> Stack	-
 	 * --> Heap	-
 	 * --> Data	-
-	 *  -->Text	-
+	 * --> Text	-
 	 * --------------------------------------->>>
 	 */
 
-	//Malloc region 0 page table memory	
+	//Malloc region 0 & 1 page table
 	kernel_page_table = malloc(MAX_PT_LEN * sizeof(pte_t));
 	pte_t *user_page_table = malloc(MAX_PT_LEN * sizeof(pte_t));
 
 	SetupRegion0(kernel_page_table);
 	SetupRegion1(user_page_table);
 
-	/* <<<--------------------------
-	 * Initialize Virtual Memory
-	 * -------------------------->>>
-	 */
-	
+	// Initialize Virtual Memory
 	TracePrintf(1, "------------------------------------- WE ARE TURNING ON VIRTUAL MEMORY NOW :) -------------------------------------\n\n");
 	WriteRegister(REG_VM_ENABLE, TRUE);
 	vm_enabled = TRUE;
-	TracePrintf(1, "##################################### VIRTUAL MEMORY SECTION IS DONE ################################################\n\n");
 
 	/* <<<------------------------------
 	 * Set up the Interrupt Vector Table
 	 * ------------------------------>>>
 	 */
 
-	// Each trap/interrupt is linked to its handler so hardware can dispatch correctly.
 	TracePrintf(1,"+++++ We are setting up the IVT and im going to call the function | CALLED KERNELSTART \n");
 	setup_trap_handler(Interrupt_Vector_Table);
 	TracePrintf(1,"+++++ We have left the function and are now going to set up region 0 | CALLED KERNELSTART\n\n");
@@ -266,6 +244,7 @@ void KernelStart(char *cmd_args[], unsigned int pmem_size, UserContext *uctxt){
 
 	//Create the idle proc or process 1
 	idle_proc_create(track, frame_count, user_page_table, uctxt);
+
 	/*
 	PCB *init_pcb = createInit();
 	if(init_pcb == NULL){
