9dcc5bb0fd5496f0ea66c52cb581dba8c629ae42
angel <angelcerv2006@gmail.com>
2025-11-09 17:17:43 -0500

    merging

--- user/forky.c
+++ user/forky.c
@@ -1,16 +1,16 @@
 #include <yalnix.h>
 #include <yuser.h>
 
-int main (void){
-        int check = Fork();
-        int status = 0;
+int main(void) {
+	int check = Fork();
+	int status = 0;
 
-        if(check == 0){
-                TtyPrintf(0, "Hallo\n");
-        }else{
-                TtyPrintf(0, "Hello friend\n");
-                Wait(&status);
-        }
-        TtyPrintf(0, "This is the value of the status -> %d and this is my pid ->%d\n", status, GetPid());
-        Exit(45);
+	if (check == 0) {
+		TtyPrintf(0, "Hallo\n");
+	} else {
+		TtyPrintf(0, "Hello friend\n");
+		Wait(&status);
+	}
+	TtyPrintf(0, "This is the value of the status -> %d and this is my pid ->%d\n", status, GetPid());
+	Exit(45);
 }
